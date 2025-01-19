2386e25f3aa79339682120b17caaeec10bd6305e
Divyansh kumar Chaturvedi <divyanshchaturvedi317@gmail.com>
2025-01-19 23:48:39 +0530

    updating WeatherApp

--- weatherProject/src/pages/WeatherPage.jsx
+++ weatherProject/src/pages/WeatherPage.jsx
@@ -2,8 +2,9 @@ import WeatherRow from "../components/WeatherRow";
 import WeatherSummary from "../components/WeatherSummary";
 
 const WeatherPage = () => {
+    const isDay = false;
     return (
-        <div className="app">
+        <div className={isDay ? "app" : "app dark"}>
             <h1 className="ui header">Weather</h1>
             <button className="ui icon button" onClick={()=>{
                 console.log("Temparture unit button was clicked.")
@@ -12,8 +13,8 @@ const WeatherPage = () => {
             </button>
             <div>
                 <WeatherSummary/>
-                <table className="ui very basic table">
-                    <thead className="table-custom">
+                <table className={`ui very basic table dark${!isDay && "dark"}`}>
+                    <thead className={`table-custom${!isDay && "dark"}`}>
                     <tr>
                         <th>date</th>
                         <th>temperature</th>
@@ -21,7 +22,6 @@ const WeatherPage = () => {
                     </tr>
                     </thead>
                     <tbody className="table-custom">
-                        <WeatherRow/>
                         <WeatherRow/>
                         <WeatherRow/>
                         <WeatherRow/>
